4c880b56890da801c95995463c5aa190044a16e0
Patrick Watson <patrickwakainos.com@TANK.local>
2020-12-16 10:28:41 +0000

    Got deck and card objects working

--- src/com/company/Deck.java
+++ src/com/company/Deck.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Random;
+
 public class Deck {
 
     private Card[] deck;
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,27 +1,37 @@
 package com.company;
 
+
 public class Main {
 
     public static void main(String[] args) {
         Deck myDeck = new Deck();
         Card[] deck = myDeck.getDeck();
 
+
         var suit = "";
+        var startPosition = 0;
+
+
         for (int i = 1; i < 5; i++) {
             if (i == 1) {
                 suit = "Diamond";
             } else if (i == 2) {
                 suit = "Heart";
+                startPosition+=13;
             } else if (i == 3) {
                 suit = "Spade";
+                startPosition+=13;
+
             } else {
                 suit = "Club";
+                startPosition+=13;
             }
             for (int j = 1; j < 14; j++) {
                 var card = setCard(suit, Integer.toString(j));
-                deck[i*j-1] = card;
+                deck[startPosition + j - 1] = card;
             }
         }
+
         for (Card card : deck) {
             if (card != null) {
                 System.out.println(card.getValue() + card.getSuit());
@@ -35,4 +45,5 @@ public class Main {
         return newCard;
 
     }
+
 }
