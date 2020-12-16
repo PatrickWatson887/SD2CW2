1a6a22b12cfd29a546a514cc35d277c10ebba544
Patrick Watson <patrickwakainos.com@TANK.local>
2020-12-16 10:44:09 +0000

    Got deck and card objects working

---
+++ src/CardGrid.java
@@ -0,0 +1,96 @@
+import java.util.Stack;
+
+public class CardGrid {
+    public Card[][] Cards = new Card[3][3];
+
+    public CardGrid() {
+
+    }
+
+    public Stack<Card> generateCardGrid(Stack<Card> cards) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                Cards[i][j] = cards.pop();
+            }
+        }
+        return cards;
+    }
+
+    public Card selectCard(int posX, int posY) {
+        return Cards[posX][posY];
+    }
+
+    public boolean checkIfAbleToAdvance(){
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                Card a = Cards[i][j];
+                for(int k=0;k<3; k++){
+                    if(checkIfEleven(a,Cards[j][k])){
+                        return true;
+                    }
+                }
+            }
+        }
+        System.out.println("No move is possible");
+        System.out.println("Game over");
+        return false;
+    }
+
+    public void printGrid(Card[] cards){
+        System.out.println("   (0)(1)(2)");
+        for(int i=0;i<3;i++){
+            System.out.print("("+i+")");
+            for(int j=0; j<3;j++){
+                if(cards[0].getSuit() == Cards[i][j].getSuit() || cards[1].getSuit() == Cards[i][j].getSuit()) {
+                    System.out.print("["+Cards[i][j].getSuit()+"]");
+                } else {
+                    System.out.print(" "+Cards[i][j].getSuit()+" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public void printGrid(){
+        System.out.println("   (0)(1)(2)");
+        for(int i=0;i<3;i++){
+            System.out.print("("+i+")");
+            for(int j=0; j<3;j++){
+                System.out.print(" "+Cards[i][j].getSuit()+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public Card[] returnHint(){
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                Card a = Cards[i][j];
+                for(int k=0;k<3; k++){
+                    if(checkIfEleven(a,Cards[j][k])){
+                      return  new Card[]{a, Cards[j][k]};
+                    }
+                }
+            }
+        }
+        System.out.println("No move is possible");
+        return null;
+    }
+
+    public boolean checkIfEleven(Card A, Card B) {
+        if (A.getValue() + B.getValue() == 11) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean checkIfEleven(Card a, Card b, Card c){
+        if(a.getValue()+b.getValue()+c.getValue() ==48){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
---
+++ src/Deck.java
@@ -0,0 +1,112 @@
+import java.util.Collections;
+import java.util.Stack;
+
+public class Deck {
+
+    private Stack<Card> deck;
+
+    public Deck() {
+        deck=new Stack<Card>();
+    }
+
+    public Stack<Card> getDeck() {
+        return deck;
+    }
+    public void setDeck(Stack<Card> newDeck) { this.deck=newDeck; }
+    public int returnDeckSize(){ return deck.size();}
+    public void addCard(Card c){deck.push(c);}
+    public void addCard(Card c, int posX, int posY){
+        c.setPrevPos(posX,posY);
+        deck.push(c);
+
+    }
+
+    public void generateDeck(){
+        Stack<Card> newDeck = new Stack<Card>();
+
+        //spades
+        newDeck.push(new Card("\uD83C\uDCA1",1, "Ace of spades"));
+        newDeck.push(new Card("\uD83C\uDCA2",2, "2 of spades"));
+        newDeck.push(new Card("\uD83C\uDCA3",3,"3 of spades"));
+        newDeck.push(new Card("\uD83C\uDCA4",4,"4 of spades"));
+        newDeck.push(new Card("\uD83C\uDCA5",5, "5 of spades"));
+        newDeck.push(new Card("\uD83C\uDCA6",6, "6 of spades"));
+        newDeck.push(new Card("\uD83C\uDCA7",7,"7 of spades"));
+        newDeck.push(new Card("\uD83C\uDCA8",8, "8 of spades"));
+        newDeck.push(new Card("\uD83C\uDCA9",9, "9 of spades"));
+        newDeck.push(new Card("\uD83C\uDCAA",10, "10 of spades"));
+        newDeck.push(new Card("\uD83C\uDCAB",15, "King of spades"));
+        newDeck.push(new Card("\uD83C\uDCAD",16, "Queen of spades"));
+        newDeck.push(new Card("\uD83C\uDCAE",17, "Jack of spades"));
+
+        //hearts
+        newDeck.push(new Card("\uD83C\uDCB1",1, "Ace of hearts"));
+        newDeck.push(new Card("\uD83C\uDCB2",2, "2 of hearts"));
+        newDeck.push(new Card("\uD83C\uDCB3",3,"3 of hearts"));
+        newDeck.push(new Card("\uD83C\uDCB4",4,"4 of hearts"));
+        newDeck.push(new Card("\uD83C\uDCBA",5, "5 of hearts"));
+        newDeck.push(new Card("\uD83C\uDCB6",6, "6 of hearts"));
+        newDeck.push(new Card("\uD83C\uDCB7",7,"7 of hearts"));
+        newDeck.push(new Card("\uD83C\uDCB8",8, "8 of hearts"));
+        newDeck.push(new Card("\uD83C\uDCB9",9, "9 of hearts"));
+        newDeck.push(new Card("\uD83C\uDCBA",10, "10 of hearts"));
+        newDeck.push(new Card("\uD83C\uDCBB",15, "King of hearts"));
+        newDeck.push(new Card("\uD83C\uDCBD",16, "Queen of hearts"));
+        newDeck.push(new Card("\uD83C\uDCBE",17, "Jack of hearts"));
+
+        //Diamonds
+        newDeck.push(new Card("\uD83C\uDCC1",1, "Ace of diamonds"));
+        newDeck.push(new Card("\uD83C\uDCC2",2, "2 of diamonds"));
+        newDeck.push(new Card("\uD83C\uDCC3",3,"3 of diamonds"));
+        newDeck.push(new Card("\uD83C\uDCC4",4,"4 of diamonds"));
+        newDeck.push(new Card("\uD83C\uDCCA",5, "5 of diamonds"));
+        newDeck.push(new Card("\uD83C\uDCC6",6, "6 of diamonds"));
+        newDeck.push(new Card("\uD83C\uDCC7",7,"7 of diamonds"));
+        newDeck.push(new Card("\uD83C\uDCC8",8, "8 of diamonds"));
+        newDeck.push(new Card("\uD83C\uDCC9",9, "9 of diamonds"));
+        newDeck.push(new Card("\uD83C\uDCCA",10, "10 of diamonds"));
+        newDeck.push(new Card("\uD83C\uDCCB",15, "King of diamonds"));
+        newDeck.push(new Card("\uD83C\uDCCD",16, "Queen of diamonds"));
+        newDeck.push(new Card("\uD83C\uDCCE",17, "Jack of diamonds"));
+
+
+        //Clubs
+        newDeck.push(new Card("\uD83C\uDCD1",1, "Ace of clubs"));
+        newDeck.push(new Card("\uD83C\uDCD2",2, "2 of clubs"));
+        newDeck.push(new Card("\uD83C\uDCD3",3,"3 of clubs"));
+        newDeck.push(new Card("\uD83C\uDCD4",4,"4 of clubs"));
+        newDeck.push(new Card("\uD83C\uDCDA",5, "5 of clubs"));
+        newDeck.push(new Card("\uD83C\uDCD6",6, "6 of clubs"));
+        newDeck.push(new Card("\uD83C\uDCD7",7,"7 of clubs"));
+        newDeck.push(new Card("\uD83C\uDCD8",8, "8 of clubs"));
+        newDeck.push(new Card("\uD83C\uDCD9",9, "9 of clubs"));
+        newDeck.push(new Card("\uD83C\uDCDA",10, "10 of clubs"));
+        newDeck.push(new Card("\uD83C\uDCDB",15, "King of clubs"));
+        newDeck.push(new Card("\uD83C\uDCDD",16, "Queen of clubs"));
+        newDeck.push(new Card("\uD83C\uDCDE",17, "Jack of clubs"));
+
+        this.deck = newDeck;
+    }
+    public void shuffleDeck(){
+        Collections.shuffle(this.deck);
+    }
+
+    public void reverse(){
+        Card[] c = new Card[returnDeckSize()];
+        Card[] data = deck.toArray(c);
+        for(int i = 0; i < returnDeckSize() / 2; i++)
+        {
+            Card temp =data[i];
+            data[i] = data[data.length - i - 1];
+            data[data.length - i - 1] = temp;
+        }
+        deck = new Stack<Card>();
+        for(Card C: data){
+            deck.push(C);
+        }
+    }
+
+
+
+
+}
